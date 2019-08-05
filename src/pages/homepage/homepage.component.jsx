a8d6a7d3988f7d026f096cbb7cf8df95c1a839b2
noe nevarez <closner_nevarez@hotmail.com>
2019-08-05 12:50:35 +0700

    update menu-items withRoute

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,14 +4,10 @@ import Directory from '../../components/directory/directory';
 
 import './homepage.styles.scss';
 
-
-const HomePage = () => {
-	return(
-	<div className='homepage'>
-	<Directory/>	
-
-	</div>
-	);
-}
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
 
 export default HomePage;
