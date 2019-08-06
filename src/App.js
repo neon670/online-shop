9acebeebd169bd0de9b0d5a992113fab649f7821
noe nevarez <closner_nevarez@hotmail.com>
2019-08-06 12:52:01 +0700

    updated firebase-sign in with google

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component{
   render(){
   return (
         <div>
-        <Header />
+        <Header currentUser={this.state.CurrentUser}/>
         <Switch>
          <Route exact path='/' component={ HomePage } />
          <Route path ='/shop' component={ ShopPage } />
