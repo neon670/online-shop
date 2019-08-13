748dd36cfeeaa5af7845c1a46a1c0899912714e4
noe nevarez <closner_nevarez@hotmail.com>
2019-08-13 19:32:47 +0800

    updated shop cart with new data, added cart utils to group items

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,7 +1,13 @@
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils'
+
 const INITIAL_STATE = {
-	hidden: true
+	hidden: true,
+	cartItems:[
+
+	]
 };
+
 const cartReducer = (state = INITIAL_STATE, action ) => {
 	switch(action.type){
 		case CartActionTypes.TOGGLE_CART_HIDDEN:
@@ -9,6 +15,11 @@ const cartReducer = (state = INITIAL_STATE, action ) => {
 				...state,
 				hidden: !state.hidden
 			}
+			case CartActionTypes.ADD_ITEM:
+				return{
+					...state,
+					cartItems: addItemToCart(state.cartItems, action.payload)
+				};
 		default:
 			return state;
 	}
