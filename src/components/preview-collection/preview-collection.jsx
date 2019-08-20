983f242f0a541e7223d91f116cc6c1be751da03d
noe nevarez <closner_nevarez@hotmail.com>
2019-08-21 00:50:06 +0700

    moved directory and shop into reducer, updated data flow for directory shop

--- src/components/preview-collection/preview-collection.jsx
+++ src/components/preview-collection/preview-collection.jsx
@@ -9,7 +9,7 @@ const PreviewCollection = ({ title, items }) => (
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
-        .map((item ) => (
+        .map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
     </div>
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import CollectionsOverview from '../../components/collection-overview/collection-overview';
+import CollectionPage from '../collection/collection';
 
-import SHOP_DATA from './shop.data.js';
-
-import PreviewCollection from '../../components/preview-collection/preview-collection';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <PreviewCollection key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
